84e82e5337120fa177063e623287048c258e1b43
Veccher <Veccher@Veccher-PC>
2018-05-19 23:56:04 -0300

    criado classe msg, implementado gets sets, construtores e webmethods

--- Chat/src/chat/ChatServer.java
+++ Chat/src/chat/ChatServer.java
@@ -1,6 +1,11 @@
 package chat;
 
 import javax.jws.WebService;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.jws.WebMethod;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
@@ -8,8 +13,6 @@ import javax.jws.soap.SOAPBinding.Style;
 @WebService
 @SOAPBinding(style = Style.RPC)
 public interface ChatServer {
-	  @WebMethod float sendMsg(float num1, float num2);
-	  @WebMethod float getMsgs(float num1, float num2);
-	  @WebMethod float acao(float num1, float num2);
-	  @WebMethod float divisao(float num1, float num2);
+	  @WebMethod void sendMsg(Mensagem msg) throws IOException;
+	  @WebMethod ArrayList<Mensagem> getMsgs(String usuario) throws FileNotFoundException, IOException, ClassNotFoundException;
 }
